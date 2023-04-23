c36fcae350fc651ba6ffe84fe1c161539d4f5eae
bunnyBites <s.mukeshkr481@gmail.com>
2023-04-23 15:58:40 +0530

    add social anchor and title

--- lib/main.dart
+++ lib/main.dart
@@ -19,15 +19,79 @@ class MyApp extends StatelessWidget {
                 radius: 50.0,
                 foregroundImage: AssetImage("images/bunny-logo.jpg"),
               ),
-              Text(
-                "Bunny Bites",
-                style: TextStyle(
-                  fontFamily: "GloriaHallelujah",
-                  fontSize: 40.0,
+              Text("Bunny Bites",
+                  style: TextStyle(
+                    fontFamily: "GloriaHallelujah",
+                    fontSize: 40.0,
+                    color: Colors.white,
+                  )),
+              Text("Flutter Developer",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5,
+                  )),
+              SizedBox(
+                height: 40.0,
+                width: 130.0,
+                child: Divider(
                   color: Colors.white,
-                )
+                  thickness: 1.5,
+                  height: 20.0,
+                ),
               ),
-              Container(width: double.infinity,)
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.all(15.0),
+                child: Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: ListTile(
+                      title: Text(
+                        "https://www.linkedin.com/in/bunny-bites/",
+                        style: TextStyle(
+                          color: Colors.deepOrange[300],
+                        ),
+                      ),
+                      tileColor: Colors.white,
+                      leading: Icon(Icons.cruelty_free,
+                          color: Colors.deepOrange[300]),
+                    )),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.all(15.0),
+                child: Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: ListTile(
+                      title: Text(
+                        "https://github.com/bunnyBites",
+                        style: TextStyle(
+                          color: Colors.deepOrange[300],
+                        ),
+                      ),
+                      tileColor: Colors.white,
+                      leading: Icon(Icons.account_tree,
+                          color: Colors.deepOrange[300]),
+                    )),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.all(15.0),
+                child: Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: ListTile(
+                      title: Text(
+                        "https://replit.com/@bunnyBites",
+                        style: TextStyle(
+                          color: Colors.deepOrange[300],
+                        ),
+                      ),
+                      tileColor: Colors.white,
+                      leading:
+                          Icon(Icons.fireplace, color: Colors.deepOrange[300]),
+                    )),
+              ),
             ],
           ),
         ),
